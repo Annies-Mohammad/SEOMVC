f0ce07b59618ab2dc5bf58c5d61dcc27885de8ac
anniesmohammad <annies.mohammad@gmail.com>
2019-02-11 17:12:30 +1100

    Added UnitTest Project

---
+++ SEO/SEO.API.UnitTests/SearchControllerUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Moq;
+using SEO.API.Controllers;
+using SEO.BusinessLogicLayer.Models.Interfaces;
+using Xunit;
+
+namespace SEO.API.UnitTests
+{
+   public class SearchControllerUnitTests
+    {
+        private Mock<ISearchURL> _mockSearchUrl;
+        private SearchController _controller;
+
+        public SearchControllerUnitTests()
+        {
+            _controller=new SearchController(_mockSearchUrl.Object);
+        }
+
+        [Fact]
+        public void Controller_Construction_Test()
+        {
+            //Assert
+            Assert.NotNull(_controller); //or
+            _controller.Should().NotBeNull();
+        }
+    }
+}
