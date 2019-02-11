9e9f2ecbfcd036c6ac8c5aeb31337d2e02fe2873
anniesmohammad <annies.mohammad@gmail.com>
2019-02-11 16:07:34 +1100

    API Tests Base

---
+++ SEO/SEO.API.FunctionalTests/TestFixture.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace Nivo.API.FunctionalTest.TestSetup
+{
+    /// <summary>
+    /// A test fixture which hosts the target project in an in-memory server.
+    /// </summary>
+    /// <typeparam name="Startup">Target project's startup type</typeparam>
+    public class TestFixture<Startup> : IDisposable
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Test server object instance
+        /// </summary>
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Client instance
+        /// </summary>
+        public HttpClient GetClient()
+        {
+            return _client;
+        }
+      
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup(typeof(Startup))
+                .UseApplicationInsights();
+
+            _server = new TestServer(builder);
+            _client = _server.CreateClient();
+            _client.Timeout = TimeSpan.FromSeconds(120);
+        }
+
+        #endregion
+
+        #region Dispose methods
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        #endregion
+
+    }
+}
