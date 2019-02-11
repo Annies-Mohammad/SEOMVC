3aa778a221388cd3658cda0b6fb5be12f777fbc7
anniesmohammad <annies.mohammad@gmail.com>
2019-02-11 14:22:20 +1100

    First method Get in search

---
+++ SEO/SEO.API/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEO.API.Controllers
+{
+    [Route("Search")]
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get(string keywords)
+        {
+             return View("Get");
+        }
+    }
+}
