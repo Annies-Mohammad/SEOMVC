a4c426d7dab82a0ea89d403254040e9cfdd81ac3
anniesmohammad <annies.mohammad@gmail.com>
2019-02-12 15:34:02 +1100

    Tried with regex main but could nt

--- SEO/SEO.WorkerService/Constants/ServiceConstants.cs
+++ SEO/SEO.WorkerService/Constants/ServiceConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class ServiceConstants
     {
-        public static string UrlPrefix = "https://www.google.com.au/search?num=39&q={0}&btnG=Search";
+        public static string UrlPrefix = "https://www.google.com.au/search?q={0}&btnG=Search";
 
         public static string SearchUrl = "https://www.infotrack.com.au/";
     }
--- SEO/SEO.WorkerService/SEOServiceLogic/SEORequestService.cs
+++ SEO/SEO.WorkerService/SEOServiceLogic/SEORequestService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Xml.Linq;
 using SEO.WorkerService.Constants;
 using SEO.WorkerService.Interfaces;
 
@@ -31,20 +34,24 @@ namespace SEO.WorkerService.SEOServiceLogic
                 {
                     string html = reader.ReadToEnd();
                     var uri = new Uri(ServiceConstants.SearchUrl);
-                    return FindPosition(html, uri);
+                    return GetPositions(html, uri);
                 }
             }
         }
 
 
 
-        public static string FindPosition(string html, Uri url)
+        public string GetPositions(string html, Uri url)
         {
-            // string lookup = "(<a class=l href=\")(\\w+[a-zA-Z0-9.-?=/]*)";
+            //  string lookup = "(<a class=l href=\")(\\w+[a-zA-Z0-9.-?=/]*)";
             string lookup = "(<a href=\")(\\w+[a-zA-Z0-9.-?=/]*)\" class=l";
-
             StringBuilder matchers = new StringBuilder();
-            MatchCollection matches = Regex.Matches(html, lookup);
+
+           MatchCollection matches = Regex.Matches(html, lookup);
+
+
+            FindURLPosition(html);
+
 
             for (int i = 0; i < matches.Count; i++)
             {
@@ -56,8 +63,44 @@ namespace SEO.WorkerService.SEOServiceLogic
                 }
             }
 
-            return matchers.ToString() ?? "0";
+            return matchers.Length > 0 ? matchers.ToString() : "0";
         }
+
+
+
+        private IList<int> FindURLPosition(string input)
+        {
+            List<int> listPositions = new List<int>();
+
+            // 1.
+            // Find all matches in html.
+            MatchCollection m1 = Regex.Matches(input, @"(<a.*?>.*?</a>)",
+                RegexOptions.Singleline);
+
+            // 2.
+            // Loop over each match.
+            foreach (Match m in m1)
+            {
+                string value = m.Groups[1].Value;
+                var i = "";
+
+                // 3.
+                // Get href attribute.
+                Match m2 = Regex.Match(value, @"href=\""(.*?)\""",
+                    RegexOptions.Singleline);
+                if (m2.Success)
+                {
+                    i = m2.Groups[1].Value;
+                }
+
+                if (i.Contains(ServiceConstants.SearchUrl))
+                    listPositions.Add(m.Index);
+
+            }
+
+            return listPositions;
+        }
+
 
     }
 }
